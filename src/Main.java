820fcd83a4f1094dc0336ff6f7b66112c27e8383
dundun <rutc98@naver.com>
2023-03-02 20:11:55 +0900

    <feature> : 백준 문제풀이 업로드
    단계별 문제풀이 4단계 1차원 배열 10번
    1546 평균 문제
    
    <footer>

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,16 @@
 import problem.*;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) throws Exception{
-        Problem p = new P_10811();
+    public static void main(String[] args) throws IOException {
+
+        Problem p = new P_1546();
 
         p.exec();
 
     }
+
 
 
 }
