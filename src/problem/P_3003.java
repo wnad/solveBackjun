fe07d7529fbee760083ff1d22f9ab0c29dc74c3e
wnad <rutc98@naver.com>
2023-03-07 23:47:11 +0900

    3003 킹, 퀸, 룩, 비숏, 나이트, 폰 문제
    단계별 문제풀이 6단계 심화1 2번

--- src/problem/P_3003.java
+++ src/problem/P_3003.java
@@ -3,8 +3,6 @@ package problem;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class P_3003 implements Problem {
