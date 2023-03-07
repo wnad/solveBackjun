fcf7bdd62bd47a53a1be74ef694789c935a4b3dd
wnad <rutc98@naver.com>
2023-03-07 23:32:01 +0900

    25083 새싹 문제
    단계별 문제풀이 6단계 심화1 1번

---
+++ src/problem/P_25083.java
@@ -0,0 +1,28 @@
+package problem;
+
+import java.io.IOException;
+
+public class P_25083 implements Problem {
+
+    /*
+    문제
+    아래 예제와 같이 새싹을 출력하시오.
+
+    입력
+    입력은 없다.
+
+    출력
+    새싹을 출력한다.
+     */
+
+    @Override
+    public void exec() throws IOException {
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+
+    }
+}
